88a779e202597ce748a7ff71911695ed0843e815
dtran <d.chan272@gmail.com>
2022-07-24 10:49:47 -0400

    - Add navigation.xml - Add navigation to GameWonFragment and GameOverFragment from the GameFragment.kt

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
@@ -17,6 +18,14 @@ class TitleFragment : Fragment() {
     ): View? {
         val binding: FragmentTitleBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        // Option #1:
+//        binding.playButton.setOnClickListener { view ->
+//            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+//        }
+
+        // Option #2:
+        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
 
         return binding.root
     }
